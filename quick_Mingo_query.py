2168e8a3a87cd664e1e4448afeba9828f0877887
GarrettArm <garrett.w.armstrong@gmail.com>
2016-02-16 16:13:24 -0600

    simple filename change

--- quick_Mingo_query.py
+++ quick_Mingo_query.py
@@ -3,21 +3,21 @@
 import pull_from_cdm as p
 import xml.etree.ElementTree as ET
 
-# p.write_xml_to_file(p.retrieve_collections_list(), 'Collections', 'List')
+# p.write_xml_to_file(p.retrieve_collections_list(), '{}_{}'.format('Collections', 'List'))
 
 alias = 'p16313coll54'  # Mingo Family ...
 
-p.write_xml_to_file(p.retrieve_collection_metadata(alias), alias, 'Collection_Metadata')
+p.write_xml_to_file(p.retrieve_collection_metadata(alias), '{}_{}'.format(alias, 'Collection_Metadata'))
 
 collection_fields = p.retrieve_collection_fields(alias)
-p.write_xml_to_file(collection_fields, alias, 'Collection_Fields')
+p.write_xml_to_file(collection_fields, '{}_{}'.format(alias, 'Collection_Fields'))
 collection_fields_tree = ET.fromstring(collection_fields)
 
 nickname_dict = p.make_nickname_dict(collection_fields_tree)
 
 fields_to_retrieve = ['source', 'dmrecord', 'dmimage', 'find']
 xml_elems_in_coll = p.retrieve_elems_in_collection(alias, fields_to_retrieve)
-p.write_xml_to_file(xml_elems_in_coll, alias, 'Elems_in_Collection')
+p.write_xml_to_file(xml_elems_in_coll, '{}_{}'.format(alias, 'Elems_in_Collection'))
 
 elems_in_coll_tree = ET.fromstring(xml_elems_in_coll)
 
@@ -35,6 +35,6 @@ for pointer, filetype in pointers_filetypes:
         item_metadata = item_metadata.replace('</{}>'.format(key), '</{}>'.format(value.replace(' ', '_').lower()))
         item_metadata = item_metadata.replace('<{}/>'.format(key), '<{}/>'.format(value.replace(' ', '_').lower()))
 
-    p.write_xml_to_file(item_metadata, alias, pointer)
+    p.write_xml_to_file(item_metadata, '{}_{}'.format(alias, pointer))
 
-    p.write_binary_to_file(p.retrieve_binaries(alias, pointer, filetype), alias, pointer, filetype)
+    p.write_binary_to_file(p.retrieve_binaries(alias, pointer, filetype), '{}_{}'.format(alias, pointer), filetype)
