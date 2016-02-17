9a3121546b59cf6e918ab5050fe52fb0bfd4f69d
GarrettArm <garrett.w.armstrong@gmail.com>
2016-02-17 17:12:43 -0600

    simple collections work, malformed xml tags work, starting work on compound collections'

--- xmlify.py
+++ xmlify.py
@@ -22,21 +22,42 @@ def xmlify_a_collection(alias):
 
     collection_etree = ET.Element(alias)
     for pointer, filetype in pointers_filetypes:
-        object_xml = xmlify_an_object(alias, pointer)
-        collection_etree.append(ET.fromstring(object_xml))
+        if filename == 'cpd':
+            compound_root = ET.Element('{}_{}'.format(alias, pointer))
+            # call depth_first search down folders()
+                # add to etree as simple object if filetype != 'cpd'
+                # add the folder level metadata as a layer on etree, then search each containing item
+                    # add to etree if containing item filetype != 'cpd'
+                    # else recurse above "folder level metadata as layer, then search each..." funct.
+            pass
+        else:
+            item_etree = xmlify_an_object(alias, pointer)
+            collection_etree.append(item_etree)
 
     p.write_xml_to_file(ET.tostring(collection_etree, encoding="unicode", method="xml"), alias)
 
 
+
 def xmlify_an_object(alias, pointer):
-    item_metadata = p.retrieve_item_metadata(alias, pointer)
+    # modify to accept alias, pointer, & subpointer for compound collections
+    xml_text = p.retrieve_item_metadata(alias, pointer)
+    xml_text = clean_up_metadata(xml_text)
+    # p.write_xml_to_file(xml_text, '{}_{}'.format(alias, pointer))
+    # p.write_binary_to_file(p.retrieve_binaries(alias, pointer, filetype), '{}_{}'.format(alias, pointer), filetype)
+    return ET.fromstring(item_metadata)
+
+def xmlify_a_compound(alias, pointer):
+    compound_meta = p.retrieve_compound_object(alias, pointer)
+
+def clean_up_metadata(xml_text):
     for key, value in lookup_coll_nicknames(alias).items():
         value = value.replace(' ', '_').lower()
-        item_metadata = item_metadata.replace('<{}>'.format(key), '<{}>'.format(value))
-        item_metadata = item_metadata.replace('</{}>'.format(key), '</{}>'.format(value))
-        item_metadata = item_metadata.replace('<{}/>'.format(key), '<{}/>'.format(value))
+        xml_text = xml_text.replace('<{}>'.format(key), '<{}>'.format(value))
+        xml_text = xml_text.replace('</{}>'.format(key), '</{}>'.format(value))
+        xml_text = xml_text.replace('<{}/>'.format(key), '<{}/>'.format(value))
+        xml_text = xml_text.replace('<{}>'.format('xml'), '<{}_{}>'.format(alias, pointer))
+        xml_text = xml_text.replace('</{}>'.format('xml'), '</{}_{}>'.format(alias, pointer))
+        xml_text = xml_text.replace('<{}/>'.format('xml'), '<{}_{}/>'.format(alias, pointer))
+    xml_text = xml_text.replace('&#x27;', '&apos;')    
+    return xml_text
 
-        item_metadata = item_metadata.replace('<{}>'.format('xml'), '<{}_{}>'.format(alias, pointer))
-        item_metadata = item_metadata.replace('</{}>'.format('xml'), '</{}_{}>'.format(alias, pointer))
-        item_metadata = item_metadata.replace('<{}/>'.format('xml'), '</{}_{}/>'.format(alias, pointer))
-    return item_metadata
