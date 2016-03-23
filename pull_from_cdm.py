526a1ba8408d041bd9b0b29e62657afd082f0a37
GarrettArm <garrett.w.armstrong@gmail.com>
2016-03-23 13:50:48 -0500

    added query cdm for pointer count, and simplified some

--- pull_from_cdm.py
+++ pull_from_cdm.py
@@ -4,50 +4,55 @@ import urllib.request
 import os
 
 
+url_prefix = 'https://server16313.contentdm.oclc.org/dmwebservices/index.php?q='
+
+
 def retrieve_collections_list():
-    url = 'https://server16313.contentdm.oclc.org/dmwebservices/index.php?q=dmGetCollectionList/xml'
+    url = '{}dmGetCollectionList/xml'.format(url_prefix)
     with urllib.request.urlopen(url) as response:
         return response.read().decode(encoding='utf-8')
 
 
 def retrieve_collection_metadata(collection_alias):
-    url = 'https://server16313.contentdm.oclc.org/dmwebservices/index.php?q=dmGetCollectionArchivalInfo/{}/xml'.format(
-        collection_alias)
+    url = '{}dmGetCollectionArchivalInfo/{}/xml'.format(url_prefix, collection_alias)
+    with urllib.request.urlopen(url) as response:
+        return response.read().decode(encoding='utf-8')
+
+
+def retrieve_collection_total_recs(collection_alias):
+    url = '{}dmQueryTotalRecs/{}|0/xml'.format(url_prefix, collection_alias)
     with urllib.request.urlopen(url) as response:
         return response.read().decode(encoding='utf-8')
 
 
 def retrieve_collection_fields(collection_alias):
-    url = 'https://server16313.contentdm.oclc.org/dmwebservices/index.php?q=dmGetCollectionFieldInfo/{}/xml'.format(
-        collection_alias)
+    url = '{}dmGetCollectionFieldInfo/{}/xml'.format(url_prefix, collection_alias)
     with urllib.request.urlopen(url) as response:
         return response.read().decode(encoding='utf-8')
 
 
 def retrieve_elems_in_collection(collection_alias, fields_list):
-    url = 'https://server16313.contentdm.oclc.org/dmwebservices/index.php?q=dmQuery/{}/0/{}/nosort/1024/1/0/0/0/0/0/0/xml'.format(
-        collection_alias, '!'.join(fields_list))
+    url = '{}dmQuery/{}/0/{}/nosort/1024/1/0/0/0/0/0/0/xml'.format(url_prefix, collection_alias, '!'.join(fields_list))
     with urllib.request.urlopen(url) as response:
         return response.read().decode(encoding='utf-8')
 
 
 def retrieve_item_metadata(collection_alias, item_pointer):
-    url = 'https://server16313.contentdm.oclc.org/dmwebservices/index.php?q=dmGetItemInfo/{}/{}/xml'.format(
-        collection_alias, item_pointer)
+    url = '{}dmGetItemInfo/{}/{}/xml'.format(url_prefix, collection_alias, item_pointer)
     with urllib.request.urlopen(url) as response:
         return response.read().decode(encoding='utf-8')
 
 
 def retrieve_binaries(collection_alias, item_pointer, filetype):
     url = 'https://cdm16313.contentdm.oclc.org/utils/getfile/collection/{}/id/{}/filename/arbitrary.{}'.format(
-        collection_alias, item_pointer, filetype)
+        collection_alias, item_pointer, filetype
+        )
     with urllib.request.urlopen(url) as response:
         return response.read()
 
 
 def retrieve_compound_object(collection_alias, item_pointer):
-    url = 'https://server16313.contentdm.oclc.org/dmwebservices/index.php?q=dmGetCompoundObjectInfo/{}/{}/xml'.format(
-        collection_alias, item_pointer)
+    url = '{}dmGetCompoundObjectInfo/{}/{}/xml'.format(url_prefix, collection_alias, item_pointer)
     with urllib.request.urlopen(url) as response:
         return response.read().decode(encoding='utf-8')
 
