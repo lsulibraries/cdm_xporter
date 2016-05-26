9369d15601c587d6c72f73ac1bed7602929df281
GarrettArm <garrett.w.armstrong@gmail.com>
2016-05-26 16:34:56 -0500

    referenced U/Z/textExportfromCDM instead of copy of this folder

--- one_off_scripts/using_AdminPanel_csv/check_adminPanel_csv.py
+++ one_off_scripts/using_AdminPanel_csv/check_adminPanel_csv.py
@@ -6,7 +6,7 @@ import csv
 
 def check_csvs_for_corruption(filename, encoding='utf-8'):
     with open(filename, 'r', encoding=encoding) as f:
-        print(filename)
+        # print(filename)
         try:
             csv_reader = csv.reader(f, delimiter='\t', quoting=csv.QUOTE_NONE)
         except:
@@ -17,14 +17,20 @@ def check_csvs_for_corruption(filename, encoding='utf-8'):
         except:
             print(filename, "doesn't seem to have column for aliases")
 
+        pointers_num = 0
         for num, row in enumerate(csv_reader):
             pointer = row[pointer_col_num]
             if not pointer.isnumeric() or (len(pointer) == 0):
                 print(pointer, "doesn't seem like a proper pointer")
+            else:
+                pointers_num += 1
+        print(pointers_num)
 
 if __name__ == '__main__':
-    csv_dir = '/home/james/Desktop/txtExportfromCDM'
+    csv_dir = '/media/james/U/txtExportfromCDM/problems/questioning'
+    # csv_dir = '/home/james/Desktop/trash'
     for file in os.listdir(csv_dir):
+        print(file)
         if os.path.isfile(os.path.join(csv_dir, file)):
             fullpath = os.path.join(csv_dir, file)
             check_csvs_for_corruption(fullpath)
