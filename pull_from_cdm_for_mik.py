99386c281edb09c52bceea420de07fededbd66e8
GarrettArm <garrett.armstrong@gmail.com>
2016-06-08 16:11:11 -0500

    moved Cached_Cdm_files directory to outside this folder at root level of U drive, so that it is more accessible

--- pull_from_cdm_for_mik.py
+++ pull_from_cdm_for_mik.py
@@ -75,42 +75,42 @@ def retrieve_parent_info(collection_alias, item_pointer, filetype):
 
 
 def write_binary_to_file(binary, alias, new_filename, filetype):
-    os.makedirs('Cached_Cdm_files/{}'.format(alias), exist_ok=True)
+    os.makedirs('../Cached_Cdm_files/{}'.format(alias), exist_ok=True)
     filename = 'Cached_Cdm_files/{}/{}.{}'.format(alias, new_filename, filetype)
     with open(filename, 'bw') as f:
         f.write(binary)
 
 
 def write_xml_to_file(xml_text, alias, new_filename):
-    os.makedirs('Cached_Cdm_files/{}'.format(alias), exist_ok=True)
-    filename = 'Cached_Cdm_files/{}/{}.xml'.format(alias, new_filename)
+    os.makedirs('../Cached_Cdm_files/{}'.format(alias), exist_ok=True)
+    filename = '../Cached_Cdm_files/{}/{}.xml'.format(alias, new_filename)
     with open(filename, 'w') as f:
         f.write(xml_text)
 
 
 def write_json_to_file(json_text, alias, new_filename):
-    os.makedirs('Cached_Cdm_files/{}'.format(alias), exist_ok=True)
-    filename = 'Cached_Cdm_files/{}/{}.json'.format(alias, new_filename)
+    os.makedirs('../Cached_Cdm_files/{}'.format(alias), exist_ok=True)
+    filename = '../Cached_Cdm_files/{}/{}.json'.format(alias, new_filename)
     with open(filename, 'w') as f:
         f.write(json_text)
 
 
 def write_admin_binary_to_file(binary, alias, new_filename, filetype):
-    os.makedirs('AdminPanel_Cdm_files/{}'.format(alias), exist_ok=True)
-    filename = 'AdminPanel_Cdm_files/{}/{}.{}'.format(alias, new_filename, filetype)
+    os.makedirs('../AdminPanel_Cdm_files/{}'.format(alias), exist_ok=True)
+    filename = '../AdminPanel_Cdm_files/{}/{}.{}'.format(alias, new_filename, filetype)
     with open(filename, 'bw') as f:
         f.write(binary)
 
 
 def write_admin_xml_to_file(xml_text, alias, new_filename):
     os.makedirs('AdminPanel_Cdm_files/{}'.format(alias), exist_ok=True)
-    filename = 'AdminPanel_Cdm_files/{}/{}.xml'.format(alias, new_filename)
+    filename = '../AdminPanel_Cdm_files/{}/{}.xml'.format(alias, new_filename)
     with open(filename, 'w') as f:
         f.write(xml_text)
 
 
 def write_admin_json_to_file(json_text, alias, new_filename):
     os.makedirs('AdminPanel_Cdm_files/{}'.format(alias), exist_ok=True)
-    filename = 'AdminPanel_Cdm_files/{}/{}.json'.format(alias, new_filename)
+    filename = '../AdminPanel_Cdm_files/{}/{}.json'.format(alias, new_filename)
     with open(filename, 'w') as f:
         f.write(json_text)
