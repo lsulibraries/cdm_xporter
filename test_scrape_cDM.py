ac922997295d4e488e98517624ae14ba7226dee0
garett armstrong <garrett.armstrong@gmail.com>
2016-08-27 14:12:55 -0500

    added some tests

--- test_scrape_cDM.py
+++ test_scrape_cDM.py
@@ -1,18 +1,39 @@
 #! /usr/bin/python3
 
-import os
-
 import pytest
 import mock
 import scrape_cDM
 
 
-# run pytest from project root with `pytest`
+''' Run pytest from project root with: `pytest` '''
+
+
+@pytest.fixture
+def make_simple_object_etree():
+    from lxml import etree as ET
+    simple_object_xmltext = """<?xml version="1.0" encoding="UTF-8"?><xml><title>Some Title</title><contac>somename@some.org</contac><creato>Some Creator</creato><contri></contri><subjec>Some subject; And Another</subjec><credit>Some credits</credit><descri>Some desctiption</descri><notes>Some notes</notes><publis>Some published</publis><date>1776-07-04</date><type>Image</type><format>An item; A format</format><identi>An Identi</identi><source>A source</source><langua>en</langua><relati>http://louisdl.louislibraries.org/cdm4/browse.php?CISOROOT=/p111111coll11</relati><sourca></sourca><covera>Some Covera</covera><coverb>1900-01-25</coverb><rights>Some rights</rights><access>2001.0001.11</access><catlog>ABC</catlog><catalo>1492-04-15</catalo><object>jcm800</object><plugin></plugin><image>100</image><imaga>4</imaga><color>Some Color</color><extent></extent><imagb>Some Imagb</imagb><file>11 KB</file><hardwa></hardwa><digiti></digiti><digita></digita><fullrs></fullrs><find>1.jp2</find><dmaccess></dmaccess><dmimage></dmimage><dmcreated>2017-01-01</dmcreated><dmmodified>2104-04-02</dmmodified><dmoclcno></dmoclcno><dmrecord>0</dmrecord><restrictionCode>1</restrictionCode><cdmfilesize>13</cdmfilesize><cdmfilesizeformatted>1.60 MB</cdmfilesizeformatted><cdmprintpdf>0</cdmprintpdf><cdmhasocr>0</cdmhasocr><cdmisnewspaper>0</cdmisnewspaper></xml>"""
+    simple_object_etree = ET.fromstring(bytes(bytearray(simple_object_xmltext, encoding='utf-8')))
+    return simple_object_etree
+
+
+@pytest.fixture
+def make_pdfpage_page_etree():
+    from lxml import etree as ET
+    pdfpage_index_filetext = """<?xml version="1.0" encoding="utf-8"?><cpd><type>Document-PDF</type><page><pagetitle>Page 1</pagetitle><pagefile>3605.pdfpage</pagefile><pageptr>3604</pageptr></page></cpd>"""
+    pdfpage_etree = ET.fromstring(bytes(bytearray(pdfpage_index_filetext, encoding='utf-8')))
+    return pdfpage_etree.find('.//page')
+
+
+@pytest.fixture
+def make_coll_total_recs_etree():
+    from lxml import etree as ET
+    coll_total_recs_filetext = """<?xml version="1.0" encoding="UTF-8"?><results><totalrecs><suggestedtopic></suggestedtopic><total>20</total></totalrecs></results>"""
+    coll_total_recs_etree = ET.fromstring(bytes(bytearray(coll_total_recs_filetext, encoding='utf-8')))
+    return coll_total_recs_etree
+
 
 def test_is_it_a_404_xml():
-    error_return_text = """<?xml version="1.0" encoding="UTF-8"?>
-<error><code>-2</code><message>Requested item not found</message><restrictionCode>-1</restrictionCode>
-</error>"""
+    error_return_text = """<?xml version="1.0" encoding="UTF-8"?><error><code>-2</code><message>Requested item not found</message><restrictionCode>-1</restrictionCode></error>"""
     assert scrape_cDM.is_it_a_404_xml(error_return_text) is True
 
 
@@ -21,32 +42,21 @@ def test_is_it_a_404_json():
     assert scrape_cDM.is_it_a_404_json(error_return_text) is True
 
 
-@pytest.fixture
-def make_pdfpage_xml():
-    from lxml import etree as ET
-    pdfpage_index_filetext = """<?xml version="1.0" encoding="utf-8"?>
-<cpd>
-  <type>Document-PDF</type>
-  <page>
-    <pagetitle>Page 1</pagetitle>
-    <pagefile>3605.pdfpage</pagefile>
-    <pageptr>3604</pageptr>
-  </page>
-</cpd>"""
-    pdfpage_etree = ET.fromstring(bytes(bytearray(pdfpage_index_filetext,
-                                                  encoding='utf-8')))
-    return pdfpage_etree.find('.//page')
-
-def test_has_pdfpage_elems(make_pdfpage_xml):
-    assert scrape_cDM.has_pdfpage_elems(make_pdfpage_xml) is True
+def test_has_pdfpage_elems(make_pdfpage_page_etree):
+    assert scrape_cDM.has_pdfpage_elems(make_pdfpage_page_etree) is True
 
 
-def test_parse_binary_original_filetype():
-    fake_filepath = os.path.join('.', 'Test_Files')
-    assert scrape_cDM.parse_binary_original_filetype(fake_filepath, 'Simple_Object') == 'jpg'
+@mock.patch('scrape_cDM.ET')
+def test_parse_binary_original_filetype(mock_ET, make_simple_object_etree):
+    mock_ET.parse.return_value = make_simple_object_etree
+    assert scrape_cDM.parse_binary_original_filetype('imag_path', 'imag_file') == 'jp2'
+    mock_ET.parse.assert_called_with('imag_path/imag_file.xml')
 
 
-def test_count_root_objects():
-    instance = scrape_cDM.ScrapeAlias('_')
-    instance.alias_dir = 'Test_Files'
-    assert instance.count_root_objects() == 456
+@mock.patch('scrape_cDM.ET')
+def test_count_root_objects(mock_ET, make_coll_total_recs_etree):
+    mock_ET.parse.return_value = make_coll_total_recs_etree
+    scrapealias = scrape_cDM.ScrapeAlias('_')
+    scrapealias.alias_dir = 'imag_alias_dir'
+    assert scrapealias.count_root_objects() == 20
+    mock_ET.parse.assert_called_with('imag_alias_dir/Collection_TotalRecs.xml')
